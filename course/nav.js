e0c24d80bf73763e71766f82374483801b01c887
group5fpt <phulocninh321@gmail.com>
2023-01-27 18:21:09 +0700

    create dropdown

--- course/nav.js
+++ course/nav.js
@@ -1,9 +1,4 @@
-function handleButtonNav(){
-    var menuBar = document.getElementById('menuBar');
-    menuBar.style.width = '250px'
-}
-
-function handleCloseNav(){
-    var close = document.getElementById('menuBar');
-    close.style.width = '0'
+function handleButtonMenuMobile(){
+    var dropdownMobile = document.getElementById('menuBar');
+    dropdownMobile.classList.toggle('show')
 }
