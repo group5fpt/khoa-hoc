d28e678fabf6ff4b9b57e1782e498fbcca1252f8
group5fpt <phulocninh321@gmail.com>
2023-01-27 17:06:12 +0700

    new code

---
+++ course/nav.js
@@ -0,0 +1,9 @@
+function handleButtonNav(){
+    var menuBar = document.getElementById('menuBar');
+    menuBar.style.width = '250px'
+}
+
+function handleCloseNav(){
+    var close = document.getElementById('menuBar');
+    close.style.width = '0'
+}
